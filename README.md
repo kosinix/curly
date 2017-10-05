0cca0ebefb4620d4490f2fc3979bd1ed5dc2f0c2
kosinix <kosinix@users.noreply.github.com>
2017-10-05 20:19:36 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# curly
+# Curly
 A simple no-nonsense CURL wrapper in PHP
+
+## Installation
+
+ 1. Download zip and extract.
+ 2. Require the autoload file:
+
+    require_once 'path/to/curly/src/autoload.php';
+
+
+## Usage
+
+### Get
+
+    use Curly\Curl;
+    
+    $curl = new Curl();
+    $result = $curl->get('http://www.google.com');
+    
+    if($result->isError()){
+    	var_dump($result->httpCode);
+    	var_dump($result->error);
+    } else {
+    	echo $result; // Uses __tostring()
+    	// or echo $result->result;
+    }
+
+### Post
+
+    use Curly\Curl;
+    
+    $curl = new Curl();
+    $result = $curl->post('http://www.google.com', array(
+        'field1' => 'value'
+    ));
+    
+    if($result->isError()){
+    	var_dump($result->httpCode);
+    	var_dump($result->error);
+    } else {
+    	echo $result; // Uses __tostring()
+    	// or echo $result->result;
+    }
---
+++ src/Curly/Curl.php
@@ -0,0 +1,65 @@
+<?php 
+namespace Curly;
+
+class Curl {
+    protected $curl;
+    public $options;
+    public function __construct($options = array()){
+        $defaults = array(
+            'CURLOPT_RETURNTRANSFER' => true,
+            'CURLOPT_FOLLOWLOCATION' => true,
+            'CURLOPT_SSL_VERIFYPEER' => false, // Insecure SSL
+            'CURLOPT_USERAGENT' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
+        );
+        $options = array_merge($defaults, $options);
+
+        $this->curl = curl_init();
+        
+        $this->reset($options);
+
+        $this->options = $options;
+        
+    }
+    public function reset($options){
+        curl_reset($this->curl);
+
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER , $options['CURLOPT_RETURNTRANSFER']);
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, $options['CURLOPT_FOLLOWLOCATION']);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, $options['CURLOPT_SSL_VERIFYPEER']);
+        curl_setopt($this->curl, CURLOPT_USERAGENT, $options['CURLOPT_USERAGENT']);
+
+    }
+    public function get($url){
+
+        $this->reset($this->options); // Prevent post and get conflicts
+
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+
+        return new CurlResult(
+            curl_exec($this->curl),
+            curl_getinfo($this->curl, CURLINFO_HTTP_CODE),
+            curl_error($this->curl)
+        );
+        
+    }
+
+    function post($url, $fields=array()){
+        
+        $this->reset($this->options); // Prevent post and get conflicts
+        
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($fields));
+
+        return new CurlResult(
+            curl_exec($this->curl),
+            curl_getinfo($this->curl, CURLINFO_HTTP_CODE),
+            curl_error($this->curl)
+        );
+    }
+
+    public function close(){
+        curl_close($this->curl); // Consume this instance
+    }
+
+}
